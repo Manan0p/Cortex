1298c968afc3881771791e46364ed98759159f87
Manan0p <imananop@gmail.com>
2026-08-16 19:07:27 +0530

    feat: initialize auth service with Firebase and MongoDB, and set up API gateway proxy

--- backend/services/auth/config/db.js
+++ backend/services/auth/config/db.js
@@ -1,1 +1,12 @@
- 
+import mongoose from "mongoose";
+
+const connectDb = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log('Database connected')
+    } catch (error) {
+        console.log(`db error ${error}`)
+    }
+}
+
+export default connectDb
