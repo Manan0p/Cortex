7704cc60b03c7922b57b6f918d14aaf9de2eab37
Manan0p <imananop@gmail.com>
2026-08-20 22:14:08 +0530

    chore: initialize frontend dependencies and project assets

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <button className='text-2xl bg-blue-500 text-white p-2 rounded-full cursor-pointer hover:bg-blue-600'>
+        Continue with google
+      </button>
+    </div>
+  )
+}
+
+export default App
